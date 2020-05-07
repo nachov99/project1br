96128995d289009348fd1e93acf03d93c7a28bd0
nachov99 <nachovillafane99@gmail.com>
2020-05-07 00:26:42 -0300

    Finished auth verification

--- application.py
+++ application.py
@@ -4,13 +4,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flas_login import UserMixin
+import requests
 
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+#if not os.getenv("DATABASE_URL"):
+#    raise RuntimeError("DATABASE_URL is not set")
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pckgaxnrveaneq:b95fe71a9ca8dfb956cf64e39a0a791951436d4ee3235f563325d284fdfddb7e@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dacuqeovgq7jhr'
+
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -18,7 +20,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"),encoding="utf8")
+#engine = create_engine(os.getenv("DATABASE_URL"),encoding="utf8")
+engine = create_engine('postgres://pckgaxnrveaneq:b95fe71a9ca8dfb956cf64e39a0a791951436d4ee3235f563325d284fdfddb7e@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dacuqeovgq7jhr')
 db = scoped_session(sessionmaker(bind=engine))
 
 #Setting up secret key.
@@ -32,10 +35,22 @@ def index():
 @app.route("/register", methods=['GET','POST'])
 def register():
     if request.method=='POST':
+
+        #Forget any user_id
+        session.clear()
+
         #Request for user data
         username=request.form.get('username')
         email = request.form.get('email')
         password=request.form.get('password')
+
+        #Ensure username was submitted
+        if not request.form.get('username'):
+            return render_template('error.html', message='Please enter a username')
+
+        #Ensure password was submitted
+        elif not request.form.get('password'):
+            return render_template('error.html', message='Please enter a password')
 
         #If this returns a user, then the username already exists in database
         userverify = db.execute("SELECT * from users where username LIKE :username",
@@ -53,14 +68,16 @@ def register():
 
         flash('Registration successful')
         return redirect(url_for('login'))
-    return render_template('register.html')
+    else:
+        return render_template("register.html")
 
 #LOGIN FORM
 @app.route("/login", methods=['GET','POST'])
-
-
 def login():
     if request.method=='POST':
+
+        #Forget any user_id
+        session.clear()
 
         #Request for user data
         username=request.form.get('username')
@@ -74,13 +91,30 @@ def login():
         elif not request.form.get('password'):
             return render_template('error.html', message='Please enter a password')
 
-        chekuser = db.execute("SELECT * from users where username LIKE :username",
+        checkuser = db.execute("SELECT * from users where username LIKE :username",
                         {'username': request.form.get('username')}).first()
 
-        if not user or not check_password_hash(user.password, password):
+        if not checkuser or not check_password_hash(checkuser.password, password):
             return render_template('error.html', message='Please check your login details and try again.')
 
-    return render_template('login.html')
+        #Remember which user has logged in
+        session['user_id'] = checkuser[0]
+        session['user_name'] = checkuser[1]
+
+        #Redirect user to home page
+        return render_template('home.html')
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """ Log user out """
+
+    # Forget any user ID
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 
 if __name__ == "__main__":
