3db98ddc19a93019e9abcf89c8474eeedf7504ca
nachov99 <nachovillafane99@gmail.com>
2020-05-07 00:40:35 -0300

    Finished auth verify and added login required for home.html

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import requests
+from flask_login import login_required
 
 app = Flask(__name__)
 
@@ -23,9 +24,6 @@ Session(app)
 #engine = create_engine(os.getenv("DATABASE_URL"),encoding="utf8")
 engine = create_engine('postgres://pckgaxnrveaneq:b95fe71a9ca8dfb956cf64e39a0a791951436d4ee3235f563325d284fdfddb7e@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dacuqeovgq7jhr')
 db = scoped_session(sessionmaker(bind=engine))
-
-#Setting up secret key.
-app.secret_key = b'H\x1b\x89\x83|\xe3\x8b:a,\xd1I\xb5\xe6\xf2D'
 
 @app.route("/")
 def index():
@@ -115,6 +113,13 @@ def logout():
 
     # Redirect user to login form
     return redirect("/")
+
+@app.route("/")
+@login_required
+def home():
+    """ Show search box """
+
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
