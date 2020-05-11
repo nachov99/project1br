5b9c53bb9910672c1e858308dbc10d2547ad1f66
nachov99 <nachovillafane99@gmail.com>
2020-05-11 16:16:31 -0300

    In the API route there is a bug

--- application.py
+++ application.py
@@ -78,10 +78,11 @@ def register():
 #LOGIN FORM
 @app.route("/login", methods=['GET','POST'])
 def login():
-    if request.method=='POST':
 
-        #Forget any user_id
-        session.clear()
+    #Forget any user_id
+    session.clear()
+
+    if request.method=='POST':
 
         #Request for user data
         username=request.form.get('username')
@@ -96,7 +97,7 @@ def login():
             return render_template('error.html', message='Please enter a password')
 
         checkuser = db.execute("SELECT * from users where username LIKE :username",
-                        {'username': request.form.get('username')}).first()
+                        {'username': username}).fetchone()
 
         if not checkuser or not check_password_hash(checkuser.password, password):
             return render_template('error.html', message='Please check your login details and try again.')
@@ -128,29 +129,133 @@ def search():
 
     #Ensure if a book was provided
     if not request.args.get("book"):
-        return render_template("error.html", message="you must provide a book.")
+        return render_template("error.html", message="You must provide a book.")
 
-    # Take input and add a wildcard
+    # With this the user can type the first 3 leters of any string
     query = "%" + request.args.get("book") + "%"
 
-    # Capitalize all words of input for search
-    # https://docs.python.org/3.7/library/stdtypes.html?highlight=title#str.title
+    #This will let the user search for the author with no need to capitalize each word.
     query = query.title()
 
-    rows = db.execute("SELECT isbn, title, author, year FROM books WHERE \
-                        isbn LIKE :query OR \
-                        title LIKE :query OR \
-                        author LIKE :query LIMIT 15",
+    rows = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn LIKE :query OR title LIKE :query OR author LIKE :query",
                         {"query": query})
 
     # Books not founded
     if rows.rowcount == 0:
         return render_template("error.html", message="we can't find books with that description.")
 
-    # Fetch all the results
+    # Search for the results
     books = rows.fetchall()
 
     return render_template("results.html", books=books)
+
+
+#@app.route("/book/<isbn>", methods = ['GET','POST'])
+@app.route("/book/<isbn>", methods=['GET','POST'])
+@login_required
+def book(isbn):
+    if request.method == 'POST':
+
+        #Save current session
+        currentUser = session["user_id"]
+
+        #Request for book database
+        rating = request.form.get('rating')
+        comment = request.form.get('comment')
+
+        #Search for book id by isbn
+        bookResult = db.execute('SELECT isbn FROM books WHERE isbn LIKE :isbn',
+                                {"isbn": isbn}).fetchone()
+
+        #Save book_id into a variable
+        book_id = bookResult
+        book_id = book_id[0]
+
+        #Check if user already submitted a review for that book
+        userSub = db.execute('SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id',
+                            {'user_id': currentUser, 'book_id': book_id})
+
+        #If review already exists
+        if userSub.rowcount == 1:
+            flash("You've already submitted a review for this book")
+            return redirect("/book" + isbn)
+
+        #Save review into
+        rating = int(rating)
+
+        db.execute('INSERT INTO reviews (user_id, book_id, comment, rating) VALUES (:user_id, :book_id, :comment, :rating)',
+                    {'user_id': currentUser, 'book_id': book_id, 'comment': comment, 'rating': rating})
+
+        db.commit()
+
+        flash('Review submitted!')
+
+        return redirect("/book/" + isbn)
+    elif request.method == 'GET':
+        row = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+
+        bookInfo = row.fetchall()
+
+        #GOODREADS REVIEWS
+
+        # Read API key from env variable
+        #key = os.getenv("GOODREADS_KEY")
+        key = 'PntQoVaB2YSKLpy9P0RGug'
+
+        # Query the api with key and ISBN as parameters
+        query = requests.get("https://www.goodreads.com/book/review_counts.json",
+                params={"key": key, "isbns": isbn})
+
+        # Convert the response to JSON
+        response = query.json()
+
+        # "Clean" the JSON before passing it to the bookInfo list
+        response = response['books'][0]
+
+        # Append it as the second element on the list. [1]
+        bookInfo.append(response)
+
+        #users review
+
+         # Search book_id by ISBN
+        row = db.execute("SELECT isbn FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+
+        # Save id into variable
+        book = row.fetchone() # (id,)
+        book = book[0]
+
+        # Fetch book reviews
+        results = db.execute("SELECT user_id, comment, rating FROM reviews WHERE book_id = :book",
+                            {"book": book})
+
+        reviews = results.fetchall()
+
+        return render_template("book.html", bookInfo=bookInfo, reviews=reviews)
+
+#API
+@app.route("/api/book/<book_id>", methods = ["GET"])
+def book_api(book_id):
+    """Return details about a single book review."""
+    # Make sure book review exists
+    results = db.execute("SELECT * FROM reviews WHERE book_id = :book_id",
+                        {"book_id": book_id})
+    review = results.fetchall()
+    if review is None:
+        return jsonify({"error": "Invalid isbn"}), 422
+
+    #Get all reviews from that book
+    reviews = db.execute("SELECT user_id, book_id, comment, rating FROM reviews WHERE book_id = :book_id",
+                        {"book_id": book_id})
+    row = reviews.fetchall()
+    return jsonify({
+            "user": row.user_id,
+            "isbn": row.book_id,
+            "comment": row.comments,
+            "rating": row.rating
+        })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- logreq.py
+++ logreq.py
@@ -1,8 +1,8 @@
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
 def login_required(f):
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
