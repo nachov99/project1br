ba31d9c5ecb94007d9db7d94eacc648b27e4509f
nachov99 <nachovillafane99@gmail.com>
2020-06-20 17:42:49 -0300

    .

--- import.py
+++ import.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # database engine object from SQLAlchemy that manages connections to the database
-engine = create_engine('postgres://pckgaxnrveaneq:b95fe71a9ca8dfb956cf64e39a0a791951436d4ee3235f563325d284fdfddb7e@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dacuqeovgq7jhr')
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 # create a 'scoped session' that ensures different users' interactions with the
 # database are kept separate
